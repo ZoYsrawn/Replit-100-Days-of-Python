a709f049314ec3c223c62d34d6371b1487eedf0d
Zo Ux <168340995+ZoYsrawn@users.noreply.github.com>
2025-06-12 11:31:25 +0530

    Add files via upload
    An exercise based on nesting ---
    Nesting is where we put an if statement within an if statement using the power of indenting. The second if statement within the first if statement must be indented and its print statement needs to be indented one more time.
    
    CHALLENGE (Day 7):
    Fake Fan Question Generator
    Wanna find out if someone else is a true superfan of the same show, movie or interest as you? Create a program that asks what someone is interested in and includes nested if statements to ask annoying follow-up questions to see if someone is the real deal!
    
    Make sure you include multiple if/elif statements and nested if statements too!
    
    tvShow = input("What is your favorite tv show? ")
    if tvShow == "peppa pig":
      print("Ugh, why?")
      faveCharacter = input("Who is your favorite character? ")
      if faveCharacter == "daddy pig":
        print("Right answer")
      else:
        print("Nah, Daddy Pig's the greatest")
    elif tvShow == "paw patrol":
      print("Aww, sad times")
    else:
      print("Yeah, that's cool and all…")

---
+++ Fake Fan Finder.py
@@ -0,0 +1,27 @@
+print(" 🕵️ FAKE FAN FINDER 👀 ")
+print("-------------------------")
+
+anime = input("What is your favorite anime? ")
+if anime == "One Piece" or anime == "one piece" or anime == "One piece" or anime == "ONE PIECE":
+    print("Oh really?!")
+    a = input("Name me any of the characters from the anime." )
+    if a == "Nami" or a == "nami":
+        print("You got that by pure chance!")
+        b = input("Ok then, what is her job on the ship?")
+        if b == "navigator" or b == "Navigator":
+            print("Hmph! Interesting!")
+            c = input("What was her first bounty then?") 
+            if c == "16,000,000 Berries" or c == "16000000 Berries" or c == "16,000,000 berries" or c == "16000000 berries":
+                print("You are a true fan!")
+            else:
+                print("See!!! FAKE One Piece fan!")
+                print("😈")
+        else:
+            print("You're bluffing, aren't you??!!! NOT a One Piece fan!")
+            print("😈")
+    else:
+        print("NO SUCH CHARACTER IN THE ANIME! BOOOOOOOO")
+        print("😈")
+else:
+    print("That is not an anime! Booooooooo")
+    print("😈")
