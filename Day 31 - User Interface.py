81016c63bcc1b6e4d048544bd1b8a77fe77cf2b9
Zo Ux <168340995+ZoYsrawn@users.noreply.github.com>
2025-06-24 18:50:02 +0530

    👉 Day 31 Challenge: User Interfaces
    1. Create a classic user interface using string manipulation.
    2. Create these two user interfaces (below) using everything you know about extensions to print statements and f-strings.
    3. The second one is a bit more tricky as it involves alignment.
    4. There are no input statements. This is all about using print and variables in interesting ways. However, you may want to create a subroutine to make the color changing easier (like you did on Day 29).
    
    
    𝗛𝗜𝗡𝗧𝗦 💡::
    a. Create a subroutine to define your colors. It can be very similar to the subroutine you used on Day 29.
    b. Assign variables to the title, text, and words like "prev", "next", "username", etc. to make it easier to align the text.
    c. You will need f-strings anytime you include a variable and text alignment and a string together.

---
+++ Day 31 - User Interface.py
@@ -0,0 +1,32 @@
+#Interface 1
+
+music = f"\033[0;31m=\033[0m=\033[0;34m= \033[1;33m MUSIC APP \033[0;34m =\033[0m=\033[0;31m=\033[0m"
+print(f"{music: ^100}")
+print()
+print("🔥▶️   Radio Gaga")
+queen = "\033[1;33mQueen"
+print(f"{queen: ^25}")
+print()
+print()
+print()
+message = "\033[0mPREV\v  \033[0;32mNEXT\v  \033[1;31mPAUSE\033[0m"
+print(message)
+
+
+#Interface 2
+
+welcome = "WELCOME TO"
+armbook = "\033[0;34m--   ARMBOOK   --\033[0m"
+defini = "\033[1;33mDefinitely not a rip off of a certain other social media site.\033[0m"
+honest = "\033[0;31mHonest.\033[0m"
+username = "Username:"
+password = "Password:"
+print(f"{welcome: ^100}")
+print(f"{armbook: ^110}")
+print()
+print(f"{defini: >400}")
+print()
+print(f"{honest: ^110}")
+print()
+print(f"{username: ^100}")
+print(f"{password: ^100}")
