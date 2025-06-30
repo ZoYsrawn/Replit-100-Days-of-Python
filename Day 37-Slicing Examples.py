9d00d5f4740ee7a42991c4d0ac355f9af5074613
Zo Ux <168340995+ZoYsrawn@users.noreply.github.com>
2025-06-30 17:46:51 +0530

    Day 37: 𝗦𝘁𝗿𝗶𝗻𝗴 𝗦𝗹𝗶𝗰𝗶𝗻𝗴: Arguments
    𝗧𝗵𝗲 𝗦𝗲𝗰𝗿𝗲𝘁 𝗧𝗵𝗶𝗿𝗱 𝗔𝗿𝗴𝘂𝗺𝗲𝗻𝘁
    Adding a third argument to the square brackets [] specifies the gap left between characters.
    string_Name[ x : y : z ]
    
    👉 Try to print every other character in the word 'hello':
    [ ]:  myString = "Hello there my friend."
    [ ]:  print(myString[0:6:2])
    
    [ ]:  # This code outputs 'Hlo' (every second character from 'Hello').
    
    👉 Can you print every third character in the whole string?
    [ ]:  myString = "Hello there my friend."
    [ ]:  print(myString[::3])
    
    [ ]:  # This code outputs 'Hltrmfe!' (every third character from the whole string).
    
    Using a negative number in the third argument can be super useful. It starts the slice from the end of the string instead of the beginning.
    
    👉 Can you print the string backwards?!
    [ ]:  myString = "Hello there my friend."
    [ ]:  print(myString[::-1])
    
    [ ]:  #This code reverses the string, outputting '.dneirf ym ereht olleH'
    
    Play around with this third secret argument. What else can you do?

---
+++ Day 37-Slicing Examples.py
@@ -0,0 +1,16 @@
+print("SLICING EXAMPLES")
+
+myString = "Hello there my friend."
+print(myString[0:6:2])
+
+# This code outputs 'Hlo' (every second character from 'Hello').
+
+myString1 = "Hello there my friend."
+print(myString1[::3])
+
+# This code outputs 'Hltrmfe!' (every third character from the whole string).
+
+myString2 = "Hello there my friend."
+print(myString2[::-1])
+
+#This code reverses the string, outputting '.dneirf ym ereht olleH'
