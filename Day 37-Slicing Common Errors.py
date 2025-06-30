60ba88ffe7a355b9e48368ec55b3cd6c88dffea4
Zo Ux <168340995+ZoYsrawn@users.noreply.github.com>
2025-06-30 17:52:25 +0530

    Day 37: 𝗦𝘁𝗿𝗶𝗻𝗴 𝗦𝗹𝗶𝗰𝗶𝗻𝗴: Common Errors
    𝗖𝗼𝗺𝗺𝗼𝗻 𝗘𝗿𝗿𝗼𝗿𝘀
    
    𝟏). 𝐈𝐭 𝐬𝐭𝐨𝐩𝐬 𝐩𝐫𝐢𝐧𝐭𝐢𝐧𝐠 𝐭𝐨𝐨 𝐞𝐚𝐫𝐥𝐲
    
    👉 Why is it printing 'Hell' instead of 'Hello'?
    [ ]:  myString = "Hello there my friend."
    [ ]:  print(myString[0:4])
    
    𝟐). 𝐈𝐭 𝐰𝐨𝐧'𝐭 𝐬𝐭𝐨𝐩 𝐩𝐫𝐢𝐧𝐭𝐢𝐧𝐠 𝐭𝐡𝐞 𝐬𝐚𝐦𝐞 𝐜𝐡𝐚𝐫𝐚𝐜𝐭𝐞𝐫
    
    👉 What is the problem here?
    [ ]:  myString1 = "Hello there my friend."
    [ ]:  print(myString1[0:4:0])
    
    NOTE: The 0 in the third argument means 'move on 0 characters in the string each time'. You've told it to print the same character again and again and again....
    
    The third argument should be at least 1.

---
+++ Day 37-Slicing Common Errors.py
@@ -0,0 +1,11 @@
+myString = "Hello there my friend."
+print(myString[0:4])
+
+#FIX
+print(myString[0:5])
+
+myString1 = "Hello there my friend."
+print(myString1[0:4:0])
+
+#FIX
+print(myString1[0:4:1])
