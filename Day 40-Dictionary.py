47613cf7220f6c4c13db8724afa6f3f2863d4112
Zo Ux <168340995+ZoYsrawn@users.noreply.github.com>
2025-07-03 22:22:51 +0530

    Day 40: 𝗗𝗶𝗰𝘁𝗶𝗼𝗻𝗮𝗿𝗶𝗲𝘀
    𝐃𝐢𝐜𝐭𝐢𝐨𝐧𝐚𝐫𝐢𝐞𝐬
    
    Dictionaries
    As you might have guessed, we love lists. However, list items are accessed in order by index number. This isn't always the way we want it to work.
    
    Dictionaries are a slightly different type of list that access data by giving each item a key. This creates what we call key:value pairs.
    
    Now we can access each item through its key, instead of having to remember what index it is at in the list.
    
    𝗖𝗿𝗲𝗮𝘁𝗶𝗻𝗴 𝗮 𝗱𝗶𝗰𝘁𝗶𝗼𝗻𝗮𝗿𝘆 - 𝗯𝗿𝗮𝗰𝗲!
    
    Curly, curly braces...
    
    To create a dictionary we start just like a list, except with curly braces {}. This dictionary will store data about a user.
    
    The data is inserted in key value pairs like this. Each pair is separated by a comma:
    👉 The first key:value pair below has "name" as the key and "David" as the value. Try it out:
    [ ]:  myUser = {"name": "David", "age": 128}
    
    𝗣𝗿𝗶𝗻𝘁𝗶𝗻𝗴 𝘁𝗵𝗲 𝗸𝗲𝘆𝘀
    
    To output (print) from a dictionary, we can use the key instead of the index. Note that we still use square brackets for accessing items (ex: ["name"]).
    👉 Let's print "name".
    [ ]:  myUser = {"name": "David", "age": 128}
    [ ]:  print(myUser["name"])
    
    [ ]:  # This code outputs 'David'.
    
    𝗖𝗵𝗮𝗻𝗴𝗶𝗻𝗴 𝗮𝗻 𝗶𝘁𝗲𝗺
    
    You can use the = syntax to change key values.
    👉 Let's change 'David' to 'The legendary David'
    [ ]:  myUser = {"name": "David", "age": 128}
    
    [ ]:  myUser["name"] = "The legendary David"
    [ ]:  print(myUser)
    
    [ ]:  # This code outputs 'name:'the legendary David', 'age':'128.
    
    Create your own dictionary!

---
+++ Day 40-Dictionary.py
@@ -0,0 +1,9 @@
+#Creating a Dictionary using curly braces {}
+myUser = {"name": "Zeb", "age": 1208}
+
+#Printing the keys
+print(myUser["name"])
+
+#Changing an item
+myUser["age"] = 1218
+print(myUser)
